7ce6cd10d0ac802c567e1cc13158380f3a1544bd
Arailym <asermagambet@mail.ru>
2024-05-02 17:03:36 +0500

    Initial Commit

---
+++ Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Арайлым Сермагамбет on 02.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
