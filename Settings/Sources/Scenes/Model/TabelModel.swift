322d086afb6e9033ef9da3d3c141dc3cc9992e6d
Arailym <asermagambet@mail.ru>
2024-05-18 17:07:41 +0500

    MVC

---
+++ Settings/Sources/Scenes/Model/TabelModel.swift
@@ -0,0 +1,8 @@
+//
+//  TabelModel.swift
+//  Settings
+//
+//  Created by Арайлым Сермагамбет on 03.05.2024.
+//
+
+import Foundation
---
+++ Settings/Sources/Scenes/View/TabelView.swift
@@ -0,0 +1,8 @@
+//
+//  TabelView.swift
+//  Settings
+//
+//  Created by Арайлым Сермагамбет on 03.05.2024.
+//
+
+import Foundation
