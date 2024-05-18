ed3fd7888420e6fb312e10c04b6929e1e0c147eb
Arailym <asermagambet@mail.ru>
2024-05-18 17:08:15 +0500

    update mvc

--- Settings/Sources/Scenes/Model/TabelModel.swift
+++ Settings/Sources/Scenes/Model/TabelModel.swift
@@ -5,4 +5,34 @@
 //  Created by Арайлым Сермагамбет on 03.05.2024.
 //
 
-import Foundation
+import UIKit
+
+struct Section {
+    let name: String
+    let options: [SettingType]
+}
+
+enum SettingType {
+    case staticCell(model: Setting)
+    case switchCell(model: SettingSwitch)
+}
+
+struct SettingSwitch {
+    var name: String
+    var icon: UIImage?
+    var iconBackgroudColor: UIColor
+    var iconNumber: UIImage?
+    var title: String?
+    var handler: (() -> Void)
+    var isOn: Bool?
+}
+
+struct Setting {
+    var name: String
+    var icon: UIImage?
+    var iconBackgroudColor: UIColor
+    var iconNumber: UIImage?
+    var title: String?
+    var handler: (() -> Void)
+    var isOn: Bool?
+}
